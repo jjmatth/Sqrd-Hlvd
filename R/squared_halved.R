cce8b914a7c67cbd95035a67ae3e72b866351713
Jeffrey Matthew <matthew.jeffrey.j@gmail.com>
2022-11-29 13:06:05 -0600

    Package

---
+++ R/squared_halved.R
@@ -0,0 +1,29 @@
+#' Create a function to return the squares of numbers
+#'
+#' Descriptions about the function
+#'
+#' @param x Input vector, must be a character vector
+#' @return a character vector that is the squared value of input vector x
+#'
+squared <- function(x) {
+  for(j in x) {
+    y <- j^2
+    print(y)
+  }
+}
+
+
+#' Create a function to return the halved value of numbers
+#'
+#' Descriptions about the function
+#'
+#' @param x Input vector, must be a character vector
+#' @return a character vector that is the halved value of input vector x
+#'
+halved <- function(x) {
+  for(j in x) {
+    y <- j/2
+    print(y)
+  }
+}
+
